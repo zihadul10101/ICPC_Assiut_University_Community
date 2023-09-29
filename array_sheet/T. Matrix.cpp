caed4f9c635ae2a2330e2bdf437f69ca539c4572
zihad <zihadul10101@gmail.com>
2023-09-29 10:55:50 +0600

    t is done

---
+++ array_sheet/T. Matrix.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int size;
+    cin>>size;
+    int arr[size][size];
+    // get input 
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+ long long sum1=0;
+  long long sum2=0;   
+
+   for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++)
+        {
+            // main diagonal
+             if (i == j){
+              sum1+=arr[i][j];
+             }
+              // Condition for secondary diagonal
+            if ((i + j) == (size - 1)){
+             sum2+=arr[i][j]; 
+            }
+        }
+    }
+//cout<<abs(sum1) + abs(sum2)<<endl;
+	cout<<abs(sum1 - sum2)<<endl;
+
+}
--- array_sheet/arry.cpp
+++ array_sheet/arry.cpp
@@ -31,40 +31,74 @@
 
 //     return 0;
 // }
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int row,column;
+
+//     cin>>row>>column;
+//     int arr[row][column];
+//     for(int i=0;i<row;i++){  //i= 0 1
+//      for(int j=0;j<column;j++) // z=0 1
+//      {
+// // i=0 j=0 0 0
+// // i=0 j=1 0 1
+// // i=1 j=0 1 0
+// // i=1 j=1 1 1
+// cin>>arr[i][j];
+//      }
+//     }
+//     int search;
+//     cin>>search;
+//     int flag=0;
+
+//     for(int i=0;i<row;i++){  //i= 0 1
+//      for(int j=0;j<column;j++) // z=0 1
+//      {
+//         if(arr[i][j]== search){
+//              flag=1;
+//               break;
+//          }
+//      }
+//     }
+//     if(flag==1){
+//         cout<<"will not take number"<<endl;
+//     }
+//     else{
+//         cout<<"will take number"<<endl;
+//     }
+// }
 #include<bits/stdc++.h>
 using namespace std;
+
 int main(){
-    int row,column;
+    int size;
+    cin>>size;
+    int arr[size][size];
+    // get input 
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+ int sum1=0;
+  int sum2=0;   
 
-    cin>>row>>column;
-    int arr[row][column];
-    for(int i=0;i<row;i++){  //i= 0 1
-     for(int j=0;j<column;j++) // z=0 1
-     {
-// i=0 j=0 0 0
-// i=0 j=1 0 1
-// i=1 j=0 1 0
-// i=1 j=1 1 1
-cin>>arr[i][j];
-     }
+   for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++)
+        {
+            // main diagonal
+             if (i == j){
+              sum1+=arr[i][j];
+             }
+              // Condition for secondary diagonal
+            if ((i + j) == (size - 1)){
+             sum2+=arr[i][j]; 
+            }
+        }
     }
-    int search;
-    cin>>search;
-    int flag=0;
+//cout<<abs(sum1) + abs(sum2)<<endl;
+	cout<<abs(sum1 - sum2)<<endl;
 
-    for(int i=0;i<row;i++){  //i= 0 1
-     for(int j=0;j<column;j++) // z=0 1
-     {
-        if(arr[i][j]== search){
-             flag=1;
-              break;
-         }
-     }
-    }
-    if(flag==1){
-        cout<<"will not take number"<<endl;
-    }
-    else{
-        cout<<"will take number"<<endl;
-    }
 }
