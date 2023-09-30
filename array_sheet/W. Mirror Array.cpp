be00d0a121f88abff59c6ce57eff248b31b4788e
zihad <zihadul10101@gmail.com>
2023-09-30 10:25:56 +0600

    Sheet #3 (Arrays)

---
+++ array_sheet/W. Mirror Array.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int size, num_elements;
+    cin >> size >> num_elements;
+
+    int input[size][num_elements];
+    int mirror[size][num_elements];
+
+    // Reading input elements
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < num_elements; ++j) {
+            cin >> input[i][j];
+        }
+    }
+
+    // Creating the mirror array
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < num_elements; ++j) {
+            mirror[i][j] = input[i][num_elements - 1 - j];
+        }
+    }
+
+    // Printing the mirror array
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < num_elements; ++j) {
+            cout << mirror[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+// ---------------------------------------------------------------------------------
+// |   i   |   j   |   input[i][j]   |   mirror[i][j]   |   Action
+// ---------------------------------------------------------------------------------
+// |   -   |   -   |       -         |        -         |   Initialize variables
+// ---------------------------------------------------------------------------------
+// |   0   |   0   |        2        |         5        |   mirror[0][0] = input[0][2]
+// |   0   |   1   |        3        |         3        |   mirror[0][1] = input[0][1]
+// |   0   |   2   |        5        |         2        |   mirror[0][2] = input[0][0]
+// ---------------------------------------------------------------------------------
+// |   1   |   0   |        7        |        20        |   mirror[1][0] = input[1][2]
+// |   1   |   1   |        9        |         9        |   mirror[1][1] = input[1][1]
+// |   1   |   2   |       20        |         7        |   mirror[1][2] = input[1][0]
+// ---------------------------------------------------------------------------------
+// |   2   |   0   |       35        |        12        |   mirror[2][0] = input[2][2]
+// |   2   |   1   |        1        |         1        |   mirror[2][1] = input[2][1]
+// |   2   |   2   |       12        |        35        |   mirror[2][2] = input[2][0]
+// ---------------------------------------------------------------------------------
--- array_sheet/arry.cpp
+++ array_sheet/arry.cpp
@@ -1,107 +1,3 @@
-// #include <iostream>
-
-// // Function to calculate the nth Fibonacci number
-// unsigned long long fibonacci(int n) {
-//     if (n <= 0) return 0;
-//     if (n == 1) return 1;
-//     unsigned long long a = 0, b = 1, c;
-//     for (int i = 2; i <= n; i++) {
-//         c = a + b;
-//         a = b;
-//         b = c;
-//     }
-//     return b;
-// }
-
-// int main() {
-//     unsigned long long date; // Date (day of the month)
-//     std::cout << "Enter a date (day of the month): ";
-//     std::cin >> date;
-
-//     int n = 0;
-//     while (fibonacci(n) < date) {
-//         n++;
-//     }
-
-//     if (fibonacci(n) == date) {
-//         std::cout << "Fibonacci Day! " << date << " is the " << n << "th Fibonacci number." << std::endl;
-//     } else {
-//         std::cout << date << " is not a Fibonacci Day." << std::endl;
-//     }
-
-//     return 0;
-// }
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main(){
-//     int row,column;
-
-//     cin>>row>>column;
-//     int arr[row][column];
-//     for(int i=0;i<row;i++){  //i= 0 1
-//      for(int j=0;j<column;j++) // z=0 1
-//      {
-// // i=0 j=0 0 0
-// // i=0 j=1 0 1
-// // i=1 j=0 1 0
-// // i=1 j=1 1 1
-// cin>>arr[i][j];
-//      }
-//     }
-//     int search;
-//     cin>>search;
-//     int flag=0;
-
-//     for(int i=0;i<row;i++){  //i= 0 1
-//      for(int j=0;j<column;j++) // z=0 1
-//      {
-//         if(arr[i][j]== search){
-//              flag=1;
-//               break;
-//          }
-//      }
-//     }
-//     if(flag==1){
-//         cout<<"will not take number"<<endl;
-//     }
-//     else{
-//         cout<<"will take number"<<endl;
-//     }
-// }
-// #include<bits/stdc++.h>
-// using namespace std;
-
-// int main(){
-//     int size;
-//     cin>>size;
-//     int arr[size][size];
-//     // get input 
-//     for(int i=0;i<size;i++){
-//         for(int j=0;j<size;j++)
-//         {
-//             cin>>arr[i][j];
-//         }
-//     }
-//  int sum1=0;
-//   int sum2=0;   
-
-//    for(int i=0;i<size;i++){
-//         for(int j=0;j<size;j++)
-//         {
-//             // main diagonal
-//              if (i == j){
-//               sum1+=arr[i][j];
-//              }
-//               // Condition for secondary diagonal
-//             if ((i + j) == (size - 1)){
-//              sum2+=arr[i][j]; 
-//             }
-//         }
-//     }
-// //cout<<abs(sum1) + abs(sum2)<<endl;
-// 	cout<<abs(sum1 - sum2)<<endl;
-
-// }
 #include <iostream>
 using namespace std;
 
@@ -109,21 +5,30 @@ int main() {
     int size, num_elements;
     cin >> size >> num_elements;
 
-    int input[size];
-    int frequency[num_elements + 1] = {0}; // Initialize frequency array with 0
+    int input[size][num_elements];
+    int mirror[size][num_elements];
 
     // Reading input elements
     for (int i = 0; i < size; ++i) {
-        cin >> input[i];
-        // Increment frequency for the corresponding element
-        frequency[input[i]]++;
+        for (int j = 0; j < num_elements; ++j) {
+            cin >> input[i][j];
+        }
     }
 
-    // Print the frequency array
-    for (int i = 1; i <= num_elements; ++i) {
-        cout << frequency[i] << endl;
+    // Creating the mirror array
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < num_elements; ++j) {
+            mirror[i][j] = input[i][num_elements - 1 - j];
+        }
     }
-    cout << endl;
+
+    // Printing the mirror array
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < num_elements; ++j) {
+            cout << mirror[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
