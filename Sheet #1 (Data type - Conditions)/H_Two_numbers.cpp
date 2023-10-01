c0a92e53aeae757f73ecfd029a0d32ae580c58ac
zihad <zihadul10101@gmail.com>
2023-10-01 22:32:42 +0600

    Sheet #1 (Data type - Conditions)

---
+++ Sheet #1 (Data type - Conditions)/H_Two_numbers.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include <cmath>
+using namespace std;
+int main(){
+    double num1,num2;
+	cin>>num1>>num2;
+    
+	// cout<<floor(num1/num2)<<endl;
+	// cout<<ceil(num1/num2)<<endl;
+	// cout<<round(num1/num2)<<endl;
+     cout<<"floor"<<" "<< num1<<" / " << num2 <<" = "<<floor(num1/num2)<<endl;
+     cout<<"ceil"<<" "<< num1<<" / " << num2 <<" = "<<ceil(num1/num2)<<endl;
+     cout<<"round"<<" "<< num1<<" / " << num2 <<" = "<<round(num1/num2)<<endl;
+//input 10 3
+//output 
+//  floor 10 / 3 = 3
+// ceil 10 / 3 = 4
+// round 10 / 3 = 3
+
+   
+}
--- Sheet #1 (Data type - Conditions)/tempCodeRunnerFile.cpp
+++ Sheet #1 (Data type - Conditions)/tempCodeRunnerFile.cpp
@@ -1,9 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-   long long int x,y,z,r;
-    cin>>x>>y>>z>>r;
-    int difference=(x*y)-(z*r);
-    cout<<"Difference"<<" = "<<difference;
-    // Difference = -10
-}
+	// cout<<floor(num1/num2)<<endl;
+	// cout<<ceil(num1/num2)<<endl;
+	// cout<<round(num1/num2)<<endl;
