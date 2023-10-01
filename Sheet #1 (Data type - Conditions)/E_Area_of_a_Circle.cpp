9d3b04d6f4dce775461fc28fa174c8798aa855c3
zihad <zihadul10101@gmail.com>
2023-10-01 21:54:50 +0600

    Sheet #1 (Data type - Conditions)

---
+++ Sheet #1 (Data type - Conditions)/E_Area_of_a_Circle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+     cout << fixed << setprecision(9);
+     // your code.
+     double r;
+     cin>>r;
+   double  Area =  3.141592653*r*r;
+     cout<<Area;
+// 12.566370612
+}
