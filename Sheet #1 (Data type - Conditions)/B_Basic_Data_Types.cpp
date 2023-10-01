023dc3290692de721fe9dd37ea27e90fc1fdf8f9
zihad <zihadul10101@gmail.com>
2023-10-01 21:08:19 +0600

    Sheet #1 (Data type - Conditions)

---
+++ Sheet #1 (Data type - Conditions)/B_Basic_Data_Types.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int numInt;
+    long long int numLong;
+    char ch;
+      float numflot;
+     double numDouble;
+
+  
+    cin>>numInt>>numLong>>ch>>numflot>>numDouble;
+    cout<<numInt<< endl;
+    cout<<numLong<< endl;
+    cout<<ch<< endl;
+    cout<<numflot<< endl;
+    cout<<numDouble<< endl;
+}
