26634db2715a5de19e71def7624432160a545050
zihad <zihadul10101@gmail.com>
2023-10-01 22:06:51 +0600

    Sheet #1 (Data type - Conditions)

---
+++ Sheet #1 (Data type - Conditions)/G_Summation_from_1_to_N.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long num;
+	cin>>num;
+	cout<<num * (num + 1)/2<<endl;
+
+    // lemit exect on test case 9
+//    long long int num,sum=0;
+//     cin>>num;
+//      for(int i=1;i<=num;i++)
+//      {
+//        sum+=i;
+//      }
+//     cout<<sum;
+   
+}
