e00d24d83a598cea5bd9cfdf2670da01b48a37b5
zihad <zihadul10101@gmail.com>
2023-10-04 12:57:57 +0600

    cotest 1

--- Contest #1/tempCodeRunnerFile.cpp
+++ Contest #1/tempCodeRunnerFile.cpp
@@ -1,14 +1,1 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    char letter;
-	cin>>letter;
-	int askChar = letter; // 97
-    if(askChar==122){
-        cout<<"a"<<endl;
-    }
-    ++letter;
-    cout<<letter<<endl;
-
-}
+// .3
