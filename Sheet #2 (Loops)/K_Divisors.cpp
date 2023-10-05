bac98917eeee79219a2a73913d6aec1265b36ae0
zihad <zihadul10101@gmail.com>
2023-10-06 00:44:36 +0600

    loop sheet

---
+++ Sheet #2 (Loops)/K_Divisors.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+ int num;
+ cin>>num;
+  for(int i=1;i<=num;i++){
+    int result=num%i;
+    if(result==0){
+        cout<<i<<endl;
+      
+       
+    }
+   
+  }
+   
+
+
+}
