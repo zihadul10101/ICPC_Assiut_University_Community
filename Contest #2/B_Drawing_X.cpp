d479d0e30f76e75af607adf9167b7caf52a8fa8d
zihad <zihadul10101@gmail.com>
2023-10-09 19:18:46 +0600

    contest #2

---
+++ Contest #2/B_Drawing_X.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int counter; // 5
+	cin>>counter;
+	for(int i=0;i<counter;i++){ //  i = 0
+		for(int z=0;z<counter;z++){ // z = 0
+			if(i == z && i != counter/2 && z != counter/2){
+				cout<<"\\";
+			} else if (i == counter/2 && z == counter/2){
+				cout<<"X";
+			} else if(z != counter/2 && i == ((counter-1)-z)){
+				cout<<"/";
+			} else {
+				cout<<"*";
+			}
+		}
+		cout<<endl;
+	}
+}
+
+// \***/
+// *\*/*
+// **X**
+// */*\*
+// /***\
+// | i   | z   | counter | Condition Check | Output |
+// | --- | --- | ------- | ---------------- | ------ |
+// | 0   | 0   | 5       | i == z           | \      |
+// | 0   | 1   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 0   | 2   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 0   | 3   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 0   | 4   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 1   | 0   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 1   | 1   | 5       | i == counter/2 && z == counter/2 | X  |
+// | 1   | 2   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 1   | 3   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 1   | 4   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 2   | 0   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 2   | 1   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 2   | 2   | 5       | i != counter/2 && z != counter/2 | \  |
+// | 2   | 3   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 2   | 4   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 3   | 0   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 3   | 1   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 3   | 2   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 3   | 3   | 5       | i == counter/2 && z == counter/2 | X  |
+// | 3   | 4   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 4   | 0   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 4   | 1   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 4   | 2   | 5       | i != counter/2 && z != counter/2 | /  |
+// | 4   | 3   | 5       | i != counter/2 && z != counter/2 | *  |
+// | 4   | 4   | 5       | i != counter/2 && z != counter/2 | *  |
---
+++ Contest #2/C_Finding_Minimums.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int size,k;
+   cin>>size>>k;
+   int min;
+	int flag = 1;
+	int counter = 0;
+   for(int i=0;i<size;i++){
+    int number;
+    cin>>number;
+
+   }
+
+
+}
