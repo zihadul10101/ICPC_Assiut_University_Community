4bd9abd2d83fd6cbbd7822c7c3d7595f7bf876c4
zihad <zihadul10101@gmail.com>
2023-10-12 22:54:24 +0600

    Sheet #7 (Recursion)

---
+++ Sheet #7 (Recursion)/A_Print_Recursion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void print(int num){ // num = 0
+	if(num == 0){
+		return;
+	}
+	cout<<"I love Recursion"<<endl;
+	return print(num-1);
+}
+int main() {
+    int num;
+	cin>>num;
+	print(num);
+}
