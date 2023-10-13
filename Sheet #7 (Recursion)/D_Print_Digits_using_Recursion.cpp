cdfed168ac0568eb2099c87a3e01b50ec1c7f4a2
zihad <zihadul10101@gmail.com>
2023-10-13 07:11:47 +0600

    debug

--- Sheet #7 (Recursion)/D_Print_Digits_using_Recursion.cpp
+++ Sheet #7 (Recursion)/D_Print_Digits_using_Recursion.cpp
@@ -6,7 +6,16 @@ void printDigit(string N) {
     if(position==N.size()){
         return;
     }
-        cout<<N[position]<<" ";
+     // Debug information
+    // cout << "Step " << position + 1 << ": ";
+    // for (int i = 0; i < N.size(); i++) {
+    //     if (i == position) {
+    //         cout << "[" << N[i] << "] ";
+    //     } else {
+    //         cout << N[i] << " ";
+    //     }
+    // }
+    //     cout<<N[position]<<" ";
         //1 2 3
         position++;
         //0+1+1
@@ -26,3 +35,9 @@ int main() {
     }
 
 }
+
+// 2
+// 123
+// Step 1: [1] 2 3 1 Step 2: 1 [2] 3 2 Step 3: 1 2 [3] 3 
+// 54321
+// Step 1: [5] 4 3 2 1 5 Step 2: 5 [4] 3 2 1 4 Step 3: 5 4 [3] 2 1 3 Step 4: 5 4 3 [2] 1 2 Step 5: 5 4 3 2 [1] 1 
