2fd32ef11064fd13f17483835681714c354a36a5
zihad <zihadul10101@gmail.com>
2023-10-15 22:52:12 +0600

    max number using recuration

---
+++ Sheet #7 (Recursion)/K_Max_Number.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxNumber(int arr[],int n)// arr(1 -3 5 4 -6), 5
+{
+ if (n == 1) return arr[0]; 
+
+    return max(arr[n-1], maxNumber(arr, n-1)); 
+
+ 
+}
+// | n   | arr[n-1] | maxNumber(arr, n-1) | Current Max | arr           |
+// | --- | -------- | -------------------- | ----------- | ------------- |
+// | 5   | -6       | 4                    | 4           | 1 -3 5 4 4   |
+// | 4   | 4        | 5                    | 5           | 1 -3 5 5 4   |
+// | 3   | 5        | 5                    | 5           | 1 -3 5 5 4   |
+// | 2   | -3       | 1                    | 5           | 1 -3 5 1 4   |
+// | 1   | 1        | 1                    | 5           | 1 -3 5 1 4   |
+
+
+int main()
+{
+  int size;
+  cin>>size;
+  int arr[size];
+  for(int i=0;i<size;i++){
+    cin>>arr[i];
+   
+}
+ cout<<maxNumber(arr, size);
+	
+}
+
+
