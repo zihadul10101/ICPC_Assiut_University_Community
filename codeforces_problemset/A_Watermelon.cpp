c12a56c311fadf6a58581b28fd1a75b7e3262573
zihad <zihadul10101@gmail.com>
2023-10-27 18:41:54 +0600

    watermelon problem

--- codeforces_problemset/A_Watermelon.cpp
+++ codeforces_problemset/A_Watermelon.cpp
@@ -1,15 +1,15 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int num;
-//     cin>>num;
-//     if(num%2==0){
-//         cout<<"YES"<<endl;
-//     }
+int main(){
+    int num;
+    cin>>num;
+    if(num%2==0 && num !=2){
+        cout<<"YES"<<endl;
+    }
     
-//     else{
-//        cout<<"NO"<<endl; 
-//     }
-// }
+    else{
+       cout<<"NO"<<endl; 
+    }
+}
 
