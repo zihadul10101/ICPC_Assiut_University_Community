54fab0bde6591e1f2945f63dd485844906ba6fc3
zihad <zihadul10101@gmail.com>
2023-11-04 19:38:46 +0600

    codeforce problem

--- codeforces_problemset/A_Next_Round.cpp
+++ codeforces_problemset/A_Next_Round.cpp
@@ -2,27 +2,21 @@
 using namespace std;
 
 int main() {
-    int num, p;
+    int num, p,flag=0;
     cin >> num >> p;
     int arr[100];
     for (int i = 1; i <= num; i++) {
         cin >> arr[i];
     }
+ 
+     for (int i = 1; i <= num; i++) {
+              if(arr[i]>=arr[p] && arr[i]>0){
+                flag++;
+        }
     
-        int first = arr[p];
-        int adv = arr[p+1];
-    //   cout<<"first"<<first<<endl;
-    //   cout<<"adv"<<adv<<endl;
-        if((first+adv>=first) && first != 0 && adv !=0){
-         
-         cout << p+1 << endl;
-        }
-        else if(first+adv<first &&  first != 0 && adv !=0){
-             cout<< arr[p] << endl; 
-        }
-        else{
-       cout<< "0" << endl;
-        }
+    }
+      cout<<flag<<endl;
+        
         
        
 
