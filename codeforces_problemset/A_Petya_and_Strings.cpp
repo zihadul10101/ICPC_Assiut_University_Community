78aea3ec5db620ee6add6223068409e0160b264b
zihad <zihadul10101@gmail.com>
2023-11-05 21:21:07 +0600

    codeforse problem

--- codeforces_problemset/A_Petya_and_Strings.cpp
+++ codeforces_problemset/A_Petya_and_Strings.cpp
@@ -15,17 +15,9 @@ for(int i=0;i<second.size();i++){
         second[i]=tolower(second[i]);
     }
 }
-int flag=0;
-for(int i=0;i<first.size();i++){
-   if(first[i]>second[i]){
-    flag=1;
-    break;
-   }
- 
-}
 if(first==second){
     cout<<"0"<<endl;
-}else if(flag==1){
+}else if(first>second){
       cout<<"1"<<endl;
 }else{
       cout<<"-1"<<endl;
