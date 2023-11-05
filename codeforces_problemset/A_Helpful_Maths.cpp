b691d5b868830c47239bae22476654d6f1ec511f
zihad <zihadul10101@gmail.com>
2023-11-05 22:42:54 +0600

    codeforce problem

---
+++ codeforces_problemset/A_Helpful_Maths.cpp
@@ -0,0 +1,20 @@
+// C++ program to sort a string of characters 
+#include<bits/stdc++.h> 
+using namespace std;  
+int main() 
+{ 
+	string s,empty=""; 
+    cin>>s;
+    for(int i=0;i<s.size();i++){
+        if(s[i] != '+'){
+            empty += s[i];
+        }
+    }
+sort(empty.begin(),empty.end());
+cout<<empty[0];
+ for(int i=1;i<empty.size();i++){
+    cout<<"+"<<empty[i];
+ }
+  cout<<endl;
+	return 0; 
+} 
