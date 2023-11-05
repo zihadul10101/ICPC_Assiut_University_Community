139ef5d3ad8d5955df8fc9287b866c33a4a11c9a
zihad <zihadul10101@gmail.com>
2023-11-05 23:11:56 +0600

    codeforce problem

---
+++ codeforces_problemset/A_Word_Capitalization.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (isupper(s[0])) {
+        cout << s << endl;
+    } else {
+        s[0] = toupper(s[0]);
+        cout << s << endl;
+    }
+
+    return 0;
+}
--- codeforces_problemset/tempCodeRunnerFile.cpp
+++ codeforces_problemset/tempCodeRunnerFile.cpp
@@ -1,34 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-
-string first,second;
-cin>>first>>second;
-for(int i=0;i<first.size();i++){
-    if(isupper(first[i])){
-        first[i]=tolower(first[i]);
-    }
-}
-for(int i=0;i<second.size();i++){
-    if(isupper(second[i])){
-        second[i]=tolower(second[i]);
-    }
-}
-int flag=0;
-for(int i=0;i<first.size();i++){
-   if(first[i]>second[i]){
-    flag=1;
-    break;
-   }
- 
-}
-if(first==second){
-    cout<<"0"<<endl;
-}else if(flag==1){
-      cout<<"1"<<endl;
-}else{
-      cout<<"-1"<<endl;
-}
-
-}
+ // for(int i=0;i<s.size();i++){
+      
+    // }
