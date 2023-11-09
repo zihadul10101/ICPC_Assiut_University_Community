5a01a833951c39e493f4207e7fecd1587fcaa29a
zihad <zihadul10101@gmail.com>
2023-11-09 18:58:46 +0600

    codeforce problem

---
+++ codeforces_problemset/A_Elephant.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int x;
+cin>>x;
+
+if(x%5==0){
+    cout<<x/5<<endl;
+}else{
+   cout<<x/5+1<<endl;  
+}
+}
+
+// #include <iostream>
+ 
+// using namespace std;
+ 
+// int main() {
+//     int x;
+//     cin >> x;
+//     cout << (x + 4) / 5 << '\n';
+// }
