08d1845b5f6e1df2e132b41d5a795c58228ddd4c
zihad <zihadul10101@gmail.com>
2023-12-30 06:48:49 +0600

    problemset in CF

--- codeforces/codeforces_problemset/A_Is_your_horseshoe_on_the_other_hoof.cpp
+++ codeforces/codeforces_problemset/A_Is_your_horseshoe_on_the_other_hoof.cpp
@@ -7,12 +7,14 @@ int main() {
      for(int i=0;i<4;i++){
         cin>>arr[i];
      }
+     // 1 2 3 2    1 2 2 3
+     sort(arr,arr+4);
      for(int i=0;i<4;i++){
        if(arr[i]==arr[i+1]){
         count++;
        }
      }
- cout<<count<<endl;
+     cout<<count<<endl;
     return 0;
 }
 
