c0c9b05b82160d1a99d9e493535461b17c26b79f
zihad <zihadul10101@gmail.com>
2024-06-12 00:14:19 +0600

    div 4 a

---
+++ codeforces/Codeforces Round 952 (Div. 4)/A_Creating_Words.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <string>
+
+int main(){
+  int t;
+  cin>>t;
+  for (int i = 0; i < t; i++)
+  {
+    string a,b;
+    cin>>a>>b;
+    cout<<b[0]<<a[1]<<a[2]<<" "<<a[0]<<b[1]<<b[2]<<endl;
+  }
+  
+ 
+}
